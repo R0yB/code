91ff4e06ba7daf19e9808a33c69118eb815ea940
rbarton <rbarton@IRL-LITRBAR04>
2016-11-25 17:12:18 +0000

    Add project files.

---
+++ ForgetTheMilk/ForgetTheMilk/Controllers/TaskController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class TaskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(Tasks);
+        }
+
+        public static readonly List<Task> Tasks = new List<Task>();
+
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            Tasks.Add(new Task { Description = task });
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ project-quiz-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project_quiz_mvc.Startup))]
+namespace project_quiz_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
